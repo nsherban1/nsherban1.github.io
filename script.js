08b7a489ae88e6f6b4d3c601ed5d715f27721d5e
nsherban1 <56939403+nsherban1@users.noreply.github.com>
2022-09-22 01:08:43 -0600

    Add files via upload

--- script.js
+++ script.js
@@ -46,3 +46,23 @@ wrapper.addEventListener("click", function () {
 //     location.href = Colombia / colombia.html;
 //   }
 // });
+//Reveal sections
+const allSections = document.querySelectorAll(".backgroundband2");
+
+const revealSection = function (entries, observer) {
+  const [entry] = entries;
+  console.log(entry);
+
+  if (!entry.isIntersecting) return;
+
+  entry.target.classList.remove("section--hidden");
+  observer.unobserve(entry.target);
+};
+const sectionObserver = new IntersectionObserver(revealSection, {
+  root: null,
+  threshold: 0.25,
+});
+allSections.forEach(function (section) {
+  sectionObserver.observe(section);
+  section.classList.add("section--hidden");
+});
